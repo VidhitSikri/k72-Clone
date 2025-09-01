c4789b6dd41227adbdeccd816798081b54fe93d1
VidhitSikri <sikrividhit@gmail.com>
2025-09-01 12:51:01 +0530

    more changes in agence page

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Agence = () => {
   return (
-    <div>
-      Agence page
-    </div>
-  )
-}
+    <div className="font-[font2]">
+      <div className="mt-[55vh]">
+        <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+          
+          Soixan7e <br /> Douze
+        </h1>
+      </div>
 
-export default Agence
+      <div>
+        <p>Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Agence;
