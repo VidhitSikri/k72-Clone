9f5cfc46b071e5650f9aedb08af35021dd909e39
VidhitSikri <sikrividhit@gmail.com>
2025-09-01 12:25:20 +0530

    more changes to home page

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font2] flex justify-center items-center gap-4  '>
-      <Link className='text-[5.5vw] border-3 px-8 pt-2 leading-[6vw] border-white rounded-full uppercase'>Projects</Link>
-      <Link className='text-[5.5vw] border-3 px-8 pt-2 leading-[6vw] border-white rounded-full uppercase'>Agence</Link> 
+    <div className='font-[font2] flex items-center justify-center gap-2 overflow-hidden mb-2'>
+
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-25 pb-2 flex items-center px-10 border-white rounded-full uppercase'>
+        <Link to='/projects' className='text-[6vw] mt-6'>
+          projets
+        </Link>
+      </div>
+      
+
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-25 pb-2 flex items-center px-10 border-white rounded-full uppercase'>
+        <Link to='/agence' className='text-[6vw] mt-6'>
+          Agence
+        </Link>
+      </div>
+
     </div>
   )
 }
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -8,7 +8,7 @@ const Video = () => {
           loop
           muted
           className="h-full w-full object-cover"
-          src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756672224~exp=1756675824~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=cdb0b26f587e5ed74d6a74df0715bf0a22b614b618000b96b2fc088e9ad0273c&r=dXMtd2VzdDE%3D"
+          src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756708870~exp=1756712470~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=284b3bcd73710292ddb35935cd043306348fbec5477a3b36736efbe7f951dbc3&r=dXMtd2VzdDE%3D"
         ></video>
     </div>
   )
